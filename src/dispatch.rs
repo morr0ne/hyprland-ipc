3bce0d0b97331640c33d6e3cd9b412293ff0ae4b
Federico Maria Morrone <contact@morrone.dev>
2023-11-24 01:24:40 +0100

    Refactored dispatcher

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -1,9 +1,13 @@
-use tokio::net::UnixStream;
+use serde::{Deserialize, Serialize};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::UnixStream,
+};
 
 use crate::Error;
 
 pub struct Dispatcher {
-    inner: UnixStream,
+    socket_path: String,
 }
 
 impl Dispatcher {
@@ -11,8 +15,68 @@ impl Dispatcher {
         let his = std::env::var("HYPRLAND_INSTANCE_SIGNATURE")
             .map_err(|_err| Error::NoInstanceSignature)?;
 
-        let socket = UnixStream::connect(format!("/tmp/hypr/{his}/.socket.sock")).await?;
-
-        Ok(Self { inner: socket })
+        Ok(Self {
+            socket_path: format!("/tmp/hypr/{his}/.socket.sock"),
+        })
     }
+
+    async fn call_command(&mut self, command: &str) -> Result<String, Error> {
+        let mut socket = UnixStream::connect(&self.socket_path).await?;
+
+        socket.write_all(command.as_bytes()).await?;
+
+        let mut response = String::new();
+
+        socket.read_to_string(&mut response).await?;
+
+        Ok(response)
+    }
+
+    pub async fn clients(&mut self) -> Result<Vec<Client>, Error> {
+        let response = self.call_command("j/clients").await?;
+
+        serde_json::from_str(&response).map_err(|_err| Error::MalformedInput) // TODO: handle the serde error properly
+    }
+
+    pub async fn toggle_floating(&mut self) -> Result<(), Error> {
+        todo!()
+    }
+
+    pub async fn pin(&mut self) -> Result<String, Error> {
+        self.call_command("/dispatch pin").await
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Client {
+    pub address: String,
+    pub mapped: bool,
+    pub hidden: bool,
+    pub at: [u32; 2],
+    pub size: [u32; 2],
+    pub workspace: Workspace,
+    pub floating: bool,
+    pub monitor: i64,
+    pub class: String,
+    pub title: String,
+    #[serde(rename = "initialClass")]
+    pub initial_class: String,
+    #[serde(rename = "initialTitle")]
+    pub initial_title: String,
+    pub pid: i64,
+    pub xwayland: bool,
+    pub pinned: bool,
+    pub fullscreen: bool,
+    #[serde(rename = "fullscreenMode")]
+    pub fullscreen_mode: i64,
+    #[serde(rename = "fakeFullscreen")]
+    pub fake_fullscreen: bool,
+    pub grouped: Vec<String>,
+    pub swallowing: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Workspace {
+    pub id: i64,
+    pub name: String,
 }
